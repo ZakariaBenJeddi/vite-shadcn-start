c9f2403199bb4aa723b7f69f92f3deb86003b0c3
ZakariaBj <zakariabenjeddi22@gmail.com>
2025-01-30 23:28:38 +0100

    login form using tailwind css shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 
 import { Button } from "./components/ui/button"
 import { Progress } from "./components/ui/progress"
+import Login from "./pages/login"
 
 
 const App = () => {
   return (
     <div className="p-5 space-y-2 bg-gray-100">
-      <Button>Click me</Button>
-      <Progress value={33} />
+      {/* <Button>Click me</Button>
+      <Progress value={33} /> */}
+      <Login/>
     </div>
   )
 }
